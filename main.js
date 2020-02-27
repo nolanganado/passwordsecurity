213cb723ed19b4edc462e0d44da9615e4730a18c
nolanganado <38250760+nolanganado@users.noreply.github.com>
2020-02-27 12:52:59 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+var topTen = "123456
+password
+12345678
+qwerty
+123456789
+12345
+1234
+111111
+1234567
+dragon";
+
+var splitTen = topTen.split(" ");
+
+var password = input("Enter a password to check...");
+
+function checkTopTen() {
+    var checkTen = document.getElementById('password').value;
+  if checkTen == topTen.split
+ {
+     alert("lol ur bad")
+ };
+ };
